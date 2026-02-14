5e392e355be211ab7fea60f8f4a12678a17b3253
DavidFlashs <adestadavid858@gmail.com>
2026-02-14 10:17:56 +0700

    Fix: True Analog Fly and Dropdown Player

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+local Window = WindUI:CreateWindow({
+    Title = "Terehub",
+    Icon = "rbxassetid://136360402262473",
+    Author = "Beta Tester",
+    Folder = "Terehub",
+    Size = UDim2.fromOffset(600, 360),
+    MinSize = Vector2.new(560, 250),
+    MaxSize = Vector2.new(950, 760),
+    Transparent = true,
+    Theme = "Indigo",
+    Resizable = true,
+    SideBarWidth = 190,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = false, 
+    ScrollBarEnabled = true,
+})
+
 local MainTab = Window:Tab({ Title = "Movement", Icon = "walking" })
 local PlayerTab = Window:Tab({ Title = "Players", Icon = "users" })
 
